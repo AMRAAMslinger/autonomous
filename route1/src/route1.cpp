8c6ac9ade63fa3653e7af7259a5f18e09a02050c
Shayne <shaynezheng54@gmail.com>
2025-10-23 19:56:36 -0400

    restart

--- route1/src/route1.cpp
+++ route1/src/route1.cpp
@@ -1,45 +1,27 @@
 #include "main.h"
+#include "pros/misc.h"
+#include "route_cp.h"
 
-//Motor Definition
-pros::MotorGroup left_motors({1,3}); //Left motor group
-pros::MotorGroup right_motors({-2,-4});  //right motor group(reversed)
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_motors({1,3});
+pros::MotorGroup right_motors ({-2,-4});
 
-//Constant Speed
-const int DRIVE_SPEED = 600;
-const int TURN_SPEED = 500;
-const int DELAY = 150;
 
-//Helper Function to Stop All Motors
-void stop_motors(){
+void move_forward(int speed, int duration_ms){
+    left_motors.move_velocity(100);
+    right_motors.move_velocity(100);
+    pros::delay(distance_ms);
+
     left_motors.move_velocity(0);
-    right_motors.move_velocity(0);
+    right_motor.move_velocity(0);
+    pros::delay(100);
 }
 
-//Function to Drive Straight(wow!)
-void drive_straight(int speed, int duration_ms){
-    left_motors.move_velocity(speed);
-    right_motors.move_velocity(speed);
-    pros::delay(250);
-    stop_motors();
+void opcontrol(){
+    while(true){
+        if (controller.get_digital(DIGITAL_A)){
+            move_forward(250,1500);
+            pros::delay(200);
+        }
+    }
 }
-
-//Function to Turn(wow!)
-void turn(int speed, int duration_ms){
-    left_motors.move_velocity(speed); //Positive = Right
-    right_motors.move_velocity(-speed); //Negative = Left
-    pros::delay(duration_ms);
-    stop_motors();
-}
-
-//Route itself
-void route(){
-    drive_straight(DRIVE_SPEED, 1500);
-    turn(TURN_SPEED, 175);
-    drive_straight(DRIVE_SPEED, 750);
-    turn(-TURN_SPEED, 175);
-    drive_straight(DRIVE_SPEED, 500);
-    turn(-TURN_SPEED, 175);
-    drive_straight(DRIVE_SPEED, 750);
-    turn(TURN_SPEED, 250);
-}
-
